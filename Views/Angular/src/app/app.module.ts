bacc95d0be1d062dfaf777edbcb004a8efd9c532
DaniGaray <degaray14@gmail.com>
2021-09-24 11:58:46 -0400

    Editar

--- Views/Angular/src/app/app.module.ts
+++ Views/Angular/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EditarUsuarioComponent } from './Edit/editar-usuario/editar-usuario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EditarUsuarioComponent
   ],
   imports: [
     BrowserModule,
