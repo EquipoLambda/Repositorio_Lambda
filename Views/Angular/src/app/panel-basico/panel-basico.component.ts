67922e319750d347b9f676461b719b14c651aac0
Juan Lindo <jlindopedroza@gmail.com>
2021-09-24 22:32:38 -0500

    Actualización Panel Básico

---
+++ Views/Angular/src/app/panel-basico/panel-basico.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-panel-basico',
+  templateUrl: './panel-basico.component.html',
+  styleUrls: ['./panel-basico.component.scss']
+})
+export class PanelBasicoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
