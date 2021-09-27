cb2981eee87eb9798f61be5550a20a228588f89a
Juan Camilo Caicedo <juan.camilo.caicedo@correounivalle.edu.co>
2021-09-27 17:36:51 -0500

    Se crea el formulario de registro de productos

--- Views/Angular/src/app/app.module.ts
+++ Views/Angular/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegistroProductosComponent } from './componentes/registro-productos/registro-productos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistroProductosComponent
   ],
   imports: [
     BrowserModule,
