d630dc314ce5e6ff0d9b2f89c1a0e0997ab2ab85
Juan Camilo Caicedo <juan.camilo.caicedo@correounivalle.edu.co>
2021-09-27 18:11:00 -0500

    Se crea el formulario de registro de ventas

--- Views/Angular/src/app/app.module.ts
+++ Views/Angular/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegistroVentasComponent } from './componentes/registro-ventas/registro-ventas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistroVentasComponent
   ],
   imports: [
     BrowserModule,
---
+++ Views/Angular/src/app/componentes/registro-ventas/registro-ventas.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-registro-ventas',
+  templateUrl: './registro-ventas.component.html',
+  styleUrls: ['./registro-ventas.component.scss']
+})
+export class RegistroVentasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
