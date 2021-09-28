2f9f740428eba6244e7011af361ce9e964a67edd
Jasmin Fuquen <56594146+JasminFuquen@users.noreply.github.com>
2021-09-27 21:27:40 -0500

    Editar Rol

--- Views/Angular/src/app/app.module.ts
+++ Views/Angular/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EditarRolComponent } from './editar-rol/editar-rol.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EditarRolComponent
   ],
   imports: [
     BrowserModule,
