fdd07fa3f1190f5fd80fdae61a6ed3a710ffce7b
Jasmin Fuquen <56594146+JasminFuquen@users.noreply.github.com>
2021-10-01 00:42:39 -0500

    Se crear proyecto de react

---
+++ react-lambda/src/App.jsx
@@ -0,0 +1,9 @@
+import React,{Fragment} from 'react'
+
+export function App() {
+  return (
+      <Fragment>
+            <h1 class="text-center">Hola chic@s bienvenidos al equipo Lambda</h1>
+      </Fragment>
+  );
+}
