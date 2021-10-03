3dc6efd9746908af2dcb3530159e73277781f0b4
Jasmin Fuquen <56594146+JasminFuquen@users.noreply.github.com>
2021-10-02 19:20:09 -0500

    Se sube Listar Productos

--- Views/Angular/src/app/app.module.ts
+++ Views/Angular/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListarProductosComponent } from './listar-productos/listar-productos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListarProductosComponent
   ],
   imports: [
     BrowserModule,
