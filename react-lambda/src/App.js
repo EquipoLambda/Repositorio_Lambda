4c0cbcdd2af8d66fbf9d8ad0c82c9ff954f2e3a8
Mauroxino <maurox488@gmail.com>
2021-10-04 18:19:20 -0500

    Migracion exitosa a React usando un componente

---
+++ react-lambda/src/App.js
@@ -0,0 +1,12 @@
+import ListarProductos2 from './Components/ListarProductos2'
+
+function App(){
+  return(
+    <div className='App'>
+
+      <ListarProductos2/>
+
+    </div>
+  )
+}
+export default App;
---
+++ react-lambda/src/Components/ListarProductos2.js
@@ -0,0 +1,65 @@
+import React from "react";
+import './ListarProductos2.css'
+
+function ListarProductos2(){
+
+    return(
+
+        <div>
+                <div className="container">
+        
+        <div className="table-wrapper">
+            
+            <div className="table-title">
+                <div className="row">
+                    <h3 className="text-center"><b>Lista de productos</b></h3>
+                </div>
+            </div>
+            <input type="text" id="Busqueda" onkeyup="myFunction()" placeholder="Ingrese un nombre para buscar..."/>
+            <table className="table table-stripped">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Descripción</th>
+                        <th>Valor unitario</th>
+                        <th>Estado</th>
+                        <th className="text-center">Editar</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>00001</td>
+                        <td>Primer Producto</td>
+                        <td>$ 5.000</td>
+                        <td className="text-success"><b>Disponible</b></td>
+                        <td className="text-center">
+							<a className="edit" title="Editar producto" data-toggle="tooltip"><i className="material-icons">&#xE254;</i></a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>00002</td>
+                        <td>Segundo Producto</td>
+                        <td>$ 10.000</td>
+                        <td className="text-danger"><b>No disponible</b></td>
+                        <td className="text-center">
+							<a className="edit" title="Editar producto" data-toggle="tooltip"><i className="material-icons">&#xE254;</i></a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>00003</td>
+                        <td>Tercer Producto</td>
+                        <td>$ 2.500</td>
+                        <td className="text-danger"><b>No disponible</b></td>
+                        <td className="text-center">
+							<a className="edit" title="Editar producto" data-toggle="tooltip"><i className="material-icons">&#xE254;</i></a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>     
+        </div>
+    )
+}
+
+export default ListarProductos2
