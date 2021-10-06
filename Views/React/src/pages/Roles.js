e38412e3594ae5dae44f6893a00ab8dcd6ccaaf3
Juan Lindo <jlindopedroza@gmail.com>
2021-10-05 23:08:34 -0500

    intento 1

---
+++ Views/React/src/pages/Roles.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Roles = () => {
+  return (
+    <div className='roles'>
+      <h1>Roles</h1>
+    </div>
+  );
+};
+
+export default Roles;
---
+++ Views/React/src/pages/Usuario.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Usuario = () => {
+  return (
+    <div className='usuario'>
+      <h1>Usuario</h1>
+    </div>
+  );
+};
+
+export default Usuario;
