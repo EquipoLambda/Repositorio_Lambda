eb9b627dda03f2857a0d143118b43049b20029fc
unknown <andrexgg9@gmail.com>
2021-10-26 16:36:50 -0500

    Se agrega barra de busqueda a todos los listar, Productos, Ventas y Usuarios

--- Frontend/src/components/SaleList.jsx
+++ Frontend/src/components/SaleList.jsx
@@ -5,6 +5,9 @@ import { getSales,deleteSale} from '../services/SaleService';
 import { Link } from 'react-router-dom';
 import { getCurrentUser } from '../services/AuthService';
 
+import InputBase from '@material-ui/core/InputBase';
+import SearchIcon from '@material-ui/icons/Search';
+
 const useStyles = makeStyles({
     table: {
         width: '90%',
@@ -17,6 +20,37 @@ const useStyles = makeStyles({
             color: '#ffffff'
         }
     },
+
+    buscar: {
+        '& > *': {
+            position: 'absolute',
+            width: '1200px',
+            height: '40px',
+            padding: '0 1px',
+            color: '#000000',
+            outline: 'none',
+            border: '1px solid #000000',
+            top: '5px',
+            right: '-640px',
+
+        }
+    },
+    btn: {
+        '& > *': {
+             
+            justifyContent: 'center',
+            alignItems: 'center',
+            width: '390px',
+            height: '50px',
+            lineHeight: '40',
+            textAlign: 'center',
+            color: 'black',
+            fontSize: '20px',
+            cursor: 'pointer'
+
+        }
+    },
+
     row: {
         '& > *': {
             fontSize: 18
@@ -51,6 +85,9 @@ export function SaleList() {
 
     const [user, setUser] = useState([]);
     const [sales, setSales] = useState([]);
+    const[tablaVentas, setTablaVentas] = useState([])
+    const [busqueda, setBusqueda]= useState("")
+
 
     useEffect(() => {
         loadSalesData();
@@ -60,7 +97,26 @@ export function SaleList() {
     const loadSalesData = async () =>{
         let response = await getSales();
         setSales(response.data.data);
+        setTablaVentas(response.data.data);
     }
+
+    const handleChange=e=>{
+        setBusqueda(e.target.value);
+        filtrar(e.target.value);
+    }
+
+
+    const filtrar=(terminoBusqueda)=>{
+        // eslint-disable-next-line array-callback-return
+        var resultadosBusqueda=tablaVentas.filter((elemento)=>{
+          if(elemento.nombreCliente.toString().toLowerCase().includes(terminoBusqueda.toLowerCase())
+          || elemento.valor.toString().toLowerCase().includes(terminoBusqueda.toLowerCase())
+          ){
+            return elemento;
+          }
+        });
+        setSales(resultadosBusqueda);
+      }
 
     const deleteSaleData = async (id) =>{
         let callbackUser = window.confirm('Esta seguro de eliminar la venta');
@@ -72,6 +128,19 @@ export function SaleList() {
 
 
     return (
+
+        <div >
+            
+        <div className={classes.btn}>
+            <SearchIcon  />
+            <InputBase className={classes.buscar} 
+            placeholder="Buscar Nombre Cliente o Valor"
+            value={busqueda}
+            onChange={handleChange}
+            />
+        </div>  
+
+
         <Table className={classes.table}>
             <TableHead>
                 <TableRow className={classes.thead}>
@@ -111,5 +180,6 @@ export function SaleList() {
                 }
             </TableBody>
         </Table>
+        </div>
     )
 }
